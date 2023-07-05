76e25fbe462f743439aec90df8f87a23736a9aeb
Joell Malone <joellmalone000@gmail.com>
2023-07-05 11:19:30 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+ var customerName = 'bob'
+ const leastFavoriteCustomer = 'sam'
+
+console.log(customerName);
+
+function upperCaseCustomerName() {
+  if(typeof customerName === 'string')
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bob';
+}
